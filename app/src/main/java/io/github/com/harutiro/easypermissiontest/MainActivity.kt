1634f788a1a04cbafdf197d1f2fe78fc76f6e6fd
harutiro <harutiro2027@outlook.jp>
2022-06-15 22:15:47 +0900

    upgrade :実際に聞いてみる部分。

--- app/src/main/java/io/github/com/harutiro/easypermissiontest/MainActivity.kt
+++ app/src/main/java/io/github/com/harutiro/easypermissiontest/MainActivity.kt
@@ -1,13 +1,40 @@
 package io.github.com.harutiro.easypermissiontest
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,EasyPermissions.PermissionCallbacks{
+
+    companion object {
+        private val REQUEST_CODE = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val permissions = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CAMERA
+        )
 
+        if (!EasyPermissions.hasPermissions(this, *permissions)) {
+            // パーミッションが許可されていない時の処理
+            EasyPermissions.requestPermissions(this, "パーミッションに関する説明", REQUEST_CODE, *permissions)
+            return
+        }
+
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, list: List<String>) {
+        recreate()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, list: List<String>) {
+        finish()
     }
 }
